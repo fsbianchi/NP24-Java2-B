5e7ac72d7259e80c7072b5745ee087e9d1fcaa0b
fsbianchi <felipe.silva.bianchi@gmail.com>
2024-03-05 22:12:20 -0300

    conteudo da aula 05/03/2024

---
+++ NP24_Java2_B/src/br/com/senaisp/bauru/felipe/secao02/Text01.java
@@ -0,0 +1,22 @@
+package br.com.senaisp.bauru.felipe.secao02;
+
+public class Text01 {
+	/**
+	 * comentário para documentação
+	 * @param args
+	 */
+    public static void main(String[] args) {
+    	//comentário de uma linha
+        System.out.println("   /\\         /\\   ");
+        /*
+         * comentário de mais de uma linha
+         */
+        System.out.println("  /  \\_______/  \\  ");
+        System.out.println(" /               \\ ");
+        System.out.println("(  /\\         /\\  )");
+        System.out.println("====     V     ====");
+        System.out.println("======(__|__)======");
+        System.out.println("  (             )  ");
+        System.out.println("   (___________)   ");
+    }    
+}
