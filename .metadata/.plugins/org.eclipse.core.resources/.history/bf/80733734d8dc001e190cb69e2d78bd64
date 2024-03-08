2d952745df7a288987289c73db8332dd81395047
fsbianchi <felipe.silva.bianchi@gmail.com>
2024-03-07 22:07:10 -0300

    Conteudo da aula 07-03-24

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/bf/80733734d8dc001e190cb69e2d78bd64
@@ -0,0 +1,13 @@
+package br.com.senaisp.bauru.felipe.secao03;
+
+public class CalculoTest2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		final double PI = 3.141516;
+		PI = 3.16;
+		System.out.println(PI);
+
+	}
+
+}
---
+++ NP24_Java2_B/src/br/com/senaisp/bauru/felipe/secao03/Chickens02.java
@@ -0,0 +1,26 @@
+package br.com.senaisp.bauru.felipe.secao03;
+
+public class Chickens02 {
+    public static void main(String[] args) {
+        //Put yout code here
+    	double dailyAvarage = 0; monthlyAverage = 0;
+    	double monthlyProfit = 0.0;
+    	
+    	//Na segunda o Fazendeiro Fred recolhe 100 ovos
+    	totalEggs += 100;
+    	//Na quarta o Fazendeiro Fred recolhe 121 ovos
+    	totalEggs += 121;
+    	//No quarta o fazendeiro Fred recolhe 117 ovos
+    	totalEggs += 117;
+    	//fazendo as contas
+    	
+    	dailyAverage = totalEggs /3;
+    	monthlyAverage =
+    	monthlyProfit =
+    	
+        System.out.println("Daily Average:   " +dailyAverage);
+        System.out.println("Monthly Average: " +monthlyAverage);
+        System.out.println("Monthly Profit:  $" +monthlyProfit);
+    }
+    
+}
