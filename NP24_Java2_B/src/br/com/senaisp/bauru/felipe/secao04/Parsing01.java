518108c84d7c907b212ed178886adccef79a7b76
fsbianchi <felipe.silva.bianchi@gmail.com>
2024-03-19 21:59:36 -0300

    conteudo da aula 19032024

---
+++ NP24_Java2_B/src/br/com/senaisp/bauru/felipe/secao04/Parsing01.java
@@ -0,0 +1,20 @@
+package br.com.senaisp.bauru.felipe.secao04;
+
+public class Parsing01 {
+    public static void main(String[] args) {
+        //Declare and intitialize 3 Strings: shirtPrice, taxRate, and gibberish
+        String shirtPrice = "15", taxRate = "0.05";
+        gibberish = '887ds7nds87dsfs';        
+        
+        //Parse shirtPrice and taxRate, and print the total tax
+        double totalTax = integer.parserInt(shirtPrice)*
+        		Double.
+        
+        
+        //Try to parse taxRate as an int
+        
+        //Try to parse gibberish as an int
+        
+    }
+    
+}
---
+++ NP24_Java2_B/src/br/com/senaisp/bauru/felipe/secao05/ExemplosJOptionPage.java
@@ -0,0 +1,16 @@
+package br.com.senaisp.bauru.felipe.secao05;
+
+import javax.swing.JOptionPane;
+
+public class ExemplosJOptionPage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String nome = JOptionPane.showInputDialog ("Digite seu nome");
+		JOptionPane.showMessageDialog (null, "Olá " + nome);
+	
+		
+
+	}
+
+}
