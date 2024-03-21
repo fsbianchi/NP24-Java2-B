e9675a88929d8698ae91403fd20c26151ce138d8
fsbianchi <felipe.silva.bianchi@gmail.com>
2024-03-20 21:59:27 -0300

    conteudo da aula 20-03-2024

---
+++ NP24_Java2_B/src/br/com/senaisp/bauru/felipe/secao05/Exercício.java
@@ -0,0 +1,16 @@
+package br.com.senaisp.bauru.felipe.secao05;
+
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+
+public class Exercício {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//inicio do dialago
+		
+	}
+
+}
--- NP24_Java2_B/src/br/com/senaisp/bauru/felipe/secao05/Input03.java
+++ NP24_Java2_B/src/br/com/senaisp/bauru/felipe/secao05/Input03.java
@@ -13,7 +13,7 @@ class Input03 {
         vlr1 = scn.nextInt();
         vlr2 = scn.nextInt();
         vlr3 = scn.nextInt();
-        total = vlr1 +vlr2+vlr3;
+        total = vlr1+vlr2+vlr3;
         String nome = scn.nextLine();
         System.out.println("Olá");
         System.out.println("Olá " + nome);
