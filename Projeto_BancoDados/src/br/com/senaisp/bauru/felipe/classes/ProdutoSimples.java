66b6badd800c4708949f9ca42161f693daf67968
fsbianchi <felipe.silva.bianchi@gmail.com>
2024-04-03 21:59:42 -0300

    Conteudo da aula 03-04-24

--- Projeto_BancoDados/src/br/com/senaisp/bauru/felipe/classes/ProdutoSimples.java
+++ Projeto_BancoDados/src/br/com/senaisp/bauru/felipe/classes/ProdutoSimples.java
@@ -12,81 +12,102 @@ public class ProdutoSimples {
 	private String descricao;
 	private int saldo;
 	private double custo;
-	
-	//Usando do conector para BD
+	//Usando o conector para BD
 	private Connection conn;
-	
-	//COnstructor
-	public ProdutoSimples() throws SQLException {
-	id = 0;
-	saldo = 0;
-	custo = 0;
-	//Instanciando o DriverManager
-	conn = DriverManager.getConnection("jdbc:sqlite:C:\\Javalibs\\dados\\NP24_JAVA2_B");
-			
+	//Constructor
+	public ProdutoSimples() throws SQLException{
+		id = 0;
+		saldo = 0;
+		custo = 0;
+		//Instanciando o DriverManager
+		conn = DriverManager
+		 .getConnection("jdbc:sqlite:C:\\Javalibs\\dados\\NP24_JAVA2_B.DB");
 	}
-	//Getters e setters
+	//Gettes e setters
 	public int getId() {
 		return id;
 	}
-
 	public void setId(int id) throws Exception {
-		if (id<0) {
-			throw new Exception("O id deve ser preenchido!");
+		if (id < 0) {
+			throw new Exception("O id não pode ser negativo!");
 		}
 		this.id = id;
 	}
-
 	public String getDescricao() {
 		return descricao;
 	}
-
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
-
 	public int getSaldo() {
 		return saldo;
 	}
-
 	public void setSaldo(int saldo) {
 		this.saldo = saldo;
 	}
-
 	public double getCusto() {
 		return custo;
 	}
-
 	public void setCusto(double custo) {
 		this.custo = custo;
+		
 	}
-	//
-	public static ProdutoSimples create(String des, int sal, double cus) throws SQLException {
+	public static ProdutoSimples create(String des,
+			int sal, double cus) throws SQLException {
 		ProdutoSimples ret = new ProdutoSimples();
+		//Setando os valores
 		ret.setDescricao(des);
 		ret.setCusto(cus);
 		ret.setSaldo(sal);
 		//Gravar no banco de dados
-		//Preparando o comando para o banco de dandos
-		Statement stm = ret.conn.prepareStatement("inset into" 
-									+ "(descricao,saldo,custo) "+ "values(?,?,?)",Statement.RETURN_GENERATED_KEYS);
-		
-		((PreparedStatement) stm).setString(1, des);
-		((PreparedStatement) stm).setInt(2, sal);
-		((PreparedStatement) stm).setDouble(3, cus);
-		//Executando
-		stm.executeUpdate(des, sal);
-		//
+		//Preparando o comando para o banco de dados
+		PreparedStatement stm = ret.conn.
+				prepareStatement("insert into "
+						+ "produto(descricao,saldo,custo) "
+						+ "values(?,?,?)",
+						Statement.RETURN_GENERATED_KEYS);
+		//Os parametros começam com 1 e não zero
+		stm.setString(1, des);
+		stm.setInt(2, sal);
+		stm.setDouble(3, cus);
+		//Executando o comando de inserção
 		int linhas = stm.executeUpdate();
-		System.out.println("Adicionei " + linhas +" no banco!");
-		ResultSet rs =stm.getGeneratedKeys();
+		System.out.println("Adicionei " + linhas + " no banco!");
+		ResultSet rs = stm.getGeneratedKeys();
 		rs.next();
 		try {
 			ret.setId(rs.getInt(1));
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
-		
+		//
 		return ret;
 	}
-}
+
+   public static ProdutoSimples findByPK(int cod) throws Exception{
+	 ProdutoSimples ret = new ProdutoSimples();
+	if (cod>0) {
+		//Preparando a instrução de select
+		PreparedStatement stmt = ret.conn.prepareStatement("selec id, descricao, saldo, custo" + " from produto where id = ?");
+		//Colocando o parametro da query
+		stmt.setInt(1, cod);
+		//Executando a quey
+		ResultSet rs = stmt.executeQuery();
+		if(rs.next()) {
+			ret.setId(rs.getInt(1));
+			ret.setDescricao(rs.getString(2));
+			ret.setSaldo(rs.getInt(3));
+			ret.setCusto(rs.getDouble(4));
+		}
+		
+		else {
+			throw new Exception("Registro não encontrado!");
+		}
+		
+	} else {
+		throw new Exception ("O codigo deve ser maior que zero! ");
+	}
+	   return ret;
+   }
+   
+}   
